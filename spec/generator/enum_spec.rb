a0e56605f56db8f644acc82839a1bbbf6d374bed
Patrick Hemmer <patrick.hemmer@gmail.com>
2013-10-19 16:30:34 -0400

    add specs for enum constants

--- spec/generator/enum_spec.rb
+++ spec/generator/enum_spec.rb
@@ -21,12 +21,19 @@ describe Generator::Enum do
     #   :second,
     #   :third,
     # ]
+    # PREFIX_FIRST = 0
+    # PREFIX_SECOND = PREFIX_FIRST + 1
+    # PREFIX_THIRD = PREFIX_SECOND + 1
 
     expect(@module.enum_type(:e)).to eq(e)
 
     expect(e[:first]).to eq(0)
     expect(e[:second]).to eq(1)
     expect(e[:third]).to eq(2)
+
+    expect(@module::PREFIX_FIRST).to eq(0)
+    expect(@module::PREFIX_SECOND).to eq(1)
+    expect(@module::PREFIX_THIRD).to eq(2)
   end
 
   it 'creates an enum starting with an assignment' do
@@ -38,12 +45,19 @@ describe Generator::Enum do
     #   :second,
     #   :third,
     # ]
+    # COMMON_FIRST = 2
+    # COMMON_SECOND = COMMON_FIRST + 1
+    # COMMON_THIRD = COMMON_SECOND + 1
 
     expect(@module.enum_type(:e_2)).to eq(e_2)
 
     expect(e_2[:first]).to eq(2)
     expect(e_2[:second]).to eq(3)
     expect(e_2[:third]).to eq(4)
+
+    expect(@module::COMMON_FIRST).to eq(2)
+    expect(@module::COMMON_SECOND).to eq(3)
+    expect(@module::COMMON_THIRD).to eq(4)
   end
 
   it 'creates an enum with assignment in the middle' do
@@ -55,12 +69,19 @@ describe Generator::Enum do
     #   :second, 5,
     #   :third,
     # ]
+    # AGAIN_FIRST = 0
+    # AGAIN_SECOND = 5
+    # AGAIN_THIRD = AGAIN_SECOND + 1
 
     expect(@module.enum_type(:e_3)).to eq(e_3)
 
     expect(e_3[:first]).to eq(0)
     expect(e_3[:second]).to eq(5)
     expect(e_3[:third]).to eq(6)
+
+    expect(@module::AGAIN_FIRST).to eq(0)
+    expect(@module::AGAIN_SECOND).to eq(5)
+    expect(@module::AGAIN_THIRD).to eq(6)
   end
 
   it 'creates numeric keys' do
@@ -72,12 +93,19 @@ describe Generator::Enum do
     #   :'1',
     #   :'2',
     # ]
+    # E3_0 = 0
+    # E3_1 = E3_0 + 1
+    # E3_2 = E3_1 + 1
 
     expect(@module.enum_type(:e_4)).to eq(e_4)
 
     expect(e_4[:'0']).to eq(0)
     expect(e_4[:'1']).to eq(1)
     expect(e_4[:'2']).to eq(2)
+
+    expect(@module::E3_0).to eq(0)
+    expect(@module::E3_1).to eq(1)
+    expect(@module::E3_2).to eq(2)
   end
 
   it 'creates a single-element enum' do
@@ -87,10 +115,13 @@ describe Generator::Enum do
     # e_5 = enum :e_5, [
     #   :key,
     # ]
+    # ABSURD_ENUM_WITH_ONE_KEY = 0
 
     expect(@module.enum_type(:e_5)).to eq(e_5)
 
     expect(e_5[:key]).to eq(0)
+
+    expect(@module::ABSURD_ENUM_WITH_ONE_KEY).to eq(0)
   end
 
   it 'creates an anonymous enum' do
@@ -102,10 +133,13 @@ describe Generator::Enum do
     #   :second,
     #   :third,
     # ]
+    # ANON_FIRST = 0
+    # ANON_SECOND = ANON_FIRST + 1
+    # ANON_THIRD = ANON_SECOND + 1
 
-    # There's not much we can test here.
-    # Because it's anonymous, there's no way to reference it.
-    # We can only verify it generates valid syntax.
+    expect(@module::ANON_FIRST).to eq(0)
+    expect(@module::ANON_SECOND).to eq(1)
+    expect(@module::ANON_THIRD).to eq(2)
   end
 
   it 'creates a typedef enum' do
@@ -117,6 +151,9 @@ describe Generator::Enum do
     #   :second,
     #   :third,
     # ]
+    # TYPEDEF_FIRST = 0
+    # TYPEDEF_SECOND = TYPEDEF_FIRST + 1
+    # TYPEDEF_THIRD = TYPEDEF_SECOND + 1
 
     expect(@module.enum_type(:e_7_t)).to be_an_instance_of(FFI::Enum)
 
@@ -125,6 +162,10 @@ describe Generator::Enum do
     expect(e_7[:first]).to eq(0)
     expect(e_7[:second]).to eq(1)
     expect(e_7[:third]).to eq(2)
+
+    expect(@module::TYPEDEF_FIRST).to eq(0)
+    expect(@module::TYPEDEF_SECOND).to eq(1)
+    expect(@module::TYPEDEF_THIRD).to eq(2)
   end
 
   it 'creates a typedef enum with a name' do
@@ -136,11 +177,18 @@ describe Generator::Enum do
     #   :second,
     #   :third,
     # ]
+    # BOTH_FIRST = 0
+    # BOTH_SECOND = BOTH_FIRST + 1
+    # BOTH_THIRD = BOTH_SECOND + 1
 
     expect(@module.enum_type(:e_8_t)).to eq(e_8)
 
     expect(e_8[:first]).to eq(0)
     expect(e_8[:second]).to eq(1)
     expect(e_8[:third]).to eq(2)
+
+    expect(@module::BOTH_FIRST).to eq(0)
+    expect(@module::BOTH_SECOND).to eq(1)
+    expect(@module::BOTH_THIRD).to eq(2)
   end
 end
